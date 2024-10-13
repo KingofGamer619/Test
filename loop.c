a83e7aba4659409d1f8da036ca8a501860fbf408
KingofGamer619 <masterhero220@gmail.com>
2024-10-13 07:04:34 +0545

    first commit

---
+++ loop.c
@@ -0,0 +1,17 @@
+// code by KingofGamer619
+// oct-13-2024
+#include <stdio.h>
+int main (void)
+{
+int i = 0; //declare value  of i is 0
+printf("Loop Start\n ");  // Display Loop Start in new line
+
+while  (i < 10)  //while when i is less than 10  so print 9 times
+{ 
+  i++; //increment by one numbber 
+printf("i: %d\n", i); //Diaplay that variable i : 0  and loop  in while using
+}
+printf("Loop End\n");
+//Display Loop End in new line
+return 0;
+}
